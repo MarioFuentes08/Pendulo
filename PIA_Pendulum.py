065b7e29d19ca2310be9ecef7606fa85d0360912
MarioFuentes08 <1866005mario@gmail.com>
2022-11-02 21:35:17 -0600

    Avance en implementacion de funciones

--- PIA_Pendulum.py
+++ PIA_Pendulum.py
@@ -12,21 +12,74 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import os
 
+"""
+###################################################################################
+FUNCIONES PARA RESOLVER ECUACIONES DIFERENCIALES
+###################################################################################
+"""
+def sim_pen_eq(t,theta_S):
+    g_S = float(gravedadS_string.get())        # Acceleration due to gravity (m/s^2)
+    L_S = float(largoS_string.get())           # Length of pendulum (m)
+    dtheta2_dt_S = + (-g_S/L_S)*np.sin(theta_S[0])
+    dtheta1_dt_S = theta_S[1]
+       
+    return [dtheta1_dt_S, dtheta2_dt_S]   
 
-
-
+"""
+###################################################################################
+FUNCIONES PARA LLAMAR 
+###################################################################################
+"""
+"""
+     theta1 is angular displacement at current time instant
+     theta2 is angular velocity at current time instant
+     dtheta2_dt is angular acceleration at current time instant
+     dtheta1_dt is rate of change of angular displacement at current time instant i.e. same as theta2 
+"""
 def penduloSimple():
-    pass
-
-
+    theta1_ini_Simple = float(amplitudS_string.get()) * math.pi / 180     # Initial angular displacement (rad)
+    theta2_ini_Simple = 0                                                 # Initial angular velocity (rad/s)
+    theta_ini_Simple = [theta1_ini_Simple, theta2_ini_Simple]
+    theta12_Simple = solve_ivp(sim_pen_eq, t_span, theta_ini_Simple, t_eval = t)
+    theta1_Simple = theta12_Simple.y[0,:]
+    theta2_Simple = theta12_Simple.y[1,:]
+    plt.plot(t,theta1_Simple,label='Angular Displacement (rad)')
+    plt.plot(t,theta2_Simple,label='Angular velocity (rad/s)')
+    plt.xlabel('Time(s)')
+    plt.ylabel('Angular Disp.(rad) and Angular Vel.(rad/s)')
+    plt.legend()
+    plt.show()    
+    
+    
+    
+    
 def penduloAmortiguado():
     pass
 
 def penduloForzadoAmortiguado():
     pass
 
+"""
+###################################################################################
+VARIABLES GLOBALES PARA LA SIMULACIÓN
+###################################################################################
+"""
+# Initial and end values
+st = 0                          # Start time (s)
+et = 30                         # End time (s)
+ts = 0.1                        # Time step (s)
+t_span = [st,et+ts]
+t = np.arange(st,et+ts,ts)
+sim_points = len(t)
+l = np.arange(0,sim_points,1)
 
 
+
+"""
+###################################################################################
+INTERFAZ
+###################################################################################
+"""
 
 root = Tk()
 root.title("El pendulo") #titulo de programa
@@ -49,18 +102,18 @@ masaA_string = StringVar()
 
 
 """ Pendulo forzado amortiguado variables"""
-amplitudA_string = StringVar()
-largoA_string = StringVar()
-gravedadA_string = StringVar()
-amortiguamientoA_string = StringVar()
-masaA_string = StringVar()
+amplitudFA_string = StringVar()
+largoFA_string = StringVar()
+gravedadFA_string = StringVar()
+amortiguamientoFA_string = StringVar()
+masaFA_string = StringVar()
 amplitudFuerzaFA_string = StringVar()
 frecuenciaFA_string = StringVar()
 
 
 """PENDULO SIMPLE"""
 
-Label(root, text="Pendulo Simple",font=("Arial",15)).pack()
+Label(root, text="Pendulo Simple",font=("Arial",12)).pack()
 
 Label(root, text="Digite al amplitud (°)",font=("Arial",10)).pack()
 Entry(root, justify="center",textvariable=amplitudS_string).pack() #amplitud
@@ -77,7 +130,7 @@ Button(root,text="Simular", command= penduloSimple).pack(pady=5) #creando boton 
 
 """PENDULO AMORTIGUADO"""
 
-Label(root, text="Pendulo Amortiguado",font=("Arial",15)).pack()
+Label(root, text="Pendulo Amortiguado",font=("Arial",12)).pack()
 
 Label(root, text="Digite al amplitud (°)",font=("Arial",10)).pack()
 Entry(root, justify="center",textvariable=amplitudA_string).pack() #amplitud
@@ -100,7 +153,7 @@ Button(root,text="Simular", command= penduloAmortiguado).pack(pady=5) #creando b
  
 """PENDULO FORZADO AMORTIGUADO"""
 
-Label(root, text="Pendulo Forzado Amortiguado",font=("Arial",15)).pack()
+Label(root, text="Pendulo Forzado Amortiguado",font=("Arial",12)).pack()
 
 Label(root, text="Digite al amplitud (°)",font=("Arial",10)).pack()
 Entry(root, justify="center",textvariable=amplitudFA_string).pack() #amplitud
